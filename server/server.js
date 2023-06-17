2441821562a32479abcd69bdd6c4bcbca49d782a
nooburger81 <brandonsebastian@gmail.com>
2023-06-17 13:13:41 -0400

    server started

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
